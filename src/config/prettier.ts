27360e32714101f604b0bf0f308f8a5e0a0d5679
Ege Tirpan <ege@evet.com>
2023-03-05 12:57:24 +0300

    initial

---
+++ src/config/prettier.ts
@@ -0,0 +1,11 @@
+import type { Config } from "prettier";
+
+export const config: Config = {
+  arrowParens: "always",
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+};
+
+export default config;
