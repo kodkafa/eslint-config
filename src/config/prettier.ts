c65a17f3ce63ce824b406d2ceb075bfb174240d3
goker <goker@cebeci.name>
2024-07-27 23:23:44 +0300

    printWidth: 100,

--- src/config/prettier.ts
+++ src/config/prettier.ts
@@ -3,7 +3,7 @@ import type { Config } from "prettier";
 export const config: Config = {
   arrowParens: "always",
   tabWidth: 2,
-  printWidth: 80,
+  printWidth: 100,
   singleQuote: true,
   jsxSingleQuote: true,
   trailingComma: "all",
